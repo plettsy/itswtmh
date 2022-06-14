8f3ef0aecb6e6468041ad3e4cdab253d5f9e6b18
plettsy <gdpletts@gmail.com>
2022-06-14 12:17:18 -0500

    Securing the API

--- stacks/index.js
+++ stacks/index.js
@@ -1,4 +1,6 @@
 import { StorageStack } from "./StorageStack";
+import { ApiStack } from "./ApiStack";
+import { AuthStack } from "./AuthStack";
 
 export default function main(app) {
   app.setDefaultFunctionProps({
@@ -8,5 +10,5 @@ export default function main(app) {
       format: "esm",
     },
   });
-  app.stack(StorageStack);
+  app.stack(StorageStack).stack(ApiStack).stack(AuthStack);
 }
